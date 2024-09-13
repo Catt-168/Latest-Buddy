5775755510489d540871a97404027ba7ecbe0f81
Catt-168 <satpaingthu2000@gmail.com>
2024-09-13 09:36:34 +0630

    feat: all features added

---
+++ src/main/java/satpaingthu/testing/buddycitest/BuddycitestApplication.java
@@ -0,0 +1,13 @@
+package satpaingthu.testing.buddycitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddycitestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddycitestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/satpaingthu/testing/buddycitest/BuddycitestApplicationTests.java
@@ -0,0 +1,21 @@
+package satpaingthu.testing.buddycitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddycitestApplicationTests {
+
+
+
+	@Test
+	void newTest(){
+		System.out.println("New Test");
+	}
+	@Test
+	void newTest2(){
+		System.out.println("New Test2");
+	}
+
+
+}
